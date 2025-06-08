e201b77a8f1cdad501fb6f6713dbe134bdd33d08
yehudagabbay <yehuda.gabbay@gmail.com>
2025-06-08 18:50:23 +0300

    fix athn2

--- game_love_client/assets/utils/authConfig.js
+++ game_love_client/assets/utils/authConfig.js
@@ -1,5 +1,3 @@
 export const googleAuthConfig = {
-  expoClientId: '302885477032-j4u1a9te7f7r6d0mktheeh09l6ltm6hf.apps.googleusercontent.com',
-  androidClientId: '302885477032-k8k7v1vfvj40iq18nbeonp1mfh4jgkju.apps.googleusercontent.com',
-  iosClientId: '302885477032-k6mj16lvhgec0h9ug8klllcvrn11t8ku.apps.googleusercontent.com',
+
 };
